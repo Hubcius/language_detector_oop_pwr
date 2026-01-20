0fb166948f1ca6dcefee370ad7fc3d20940eedac
MikolajSzychowski <mszychow@gmail.com>
2026-01-20 16:21:06 +0100

    random

--- src/WebScrapperGui.java
+++ src/WebScrapperGui.java
@@ -1,17 +1,20 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.util.Map;
 
 public class WebScrapperGui
 {
     public static JFrame frame;
     public static JTextArea logArea;
     public static JTextField inputField;
+    static JComboBox c1;
 
     public static void scrapperGui()
     {
-        frame = new JFrame("WebScrapper");
+        frame = new JFrame("WebScraper");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
+        frame.setSize(750, 500);
         frame.setLayout(null);
         frame.setVisible(true);
 
@@ -38,16 +41,46 @@ public class WebScrapperGui
         startButton.setFont(new Font("Arial", Font.PLAIN, 20));
         startButton.setBounds(240, 400, 200, 50);
         startButton.addActionListener(e ->
-
-                new Thread(() -> {
-                    WebScrapper.scrapWikipedia(Integer.parseInt(inputField.getText()));
-                }).start());
-
+        {
+            new Thread(() ->
+            {
+                String langCode = "";
+                if(c1.getSelectedItem().toString().equals("All"))
+                {
+                    WebScrapper.scrapWikipedia(Integer.parseInt(inputField.getText()), "All");
+                }
+                else
+                {
+                    for (Map.Entry<String, String> entry : WebScrapper.localNames.entrySet())
+                    {
+                        if (entry.getValue().equals(c1.getSelectedItem().toString()))
+                        {
+                            langCode =  entry.getKey();
+                            break;
+                        }
+                    }
+                    WebScrapper.scrapWikipedia(Integer.parseInt(inputField.getText()), langCode);
+                }
+            }).start();
+        });
 
         frame.add(startButton);
 
+
+        JLabel label2 = new JLabel("Language to scrap");
+        label2.setFont(new Font("Arial", Font.PLAIN, 20));
+        label2.setBounds(210, 60, 200, 30);
+        frame.add(label2);
+
+
+        // create checkbox
+        c1 = new JComboBox(WebScrapper.nameList.toArray());
+        c1.setBounds(240, 100, 200, 30);
+        frame.add(c1);
+
         frame.repaint();
     }
+
 
     public static void addLog(String text)
     {
