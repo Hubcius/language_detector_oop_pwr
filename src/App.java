4a0ac3e21d1f2046c382c529fa8f4e8c0a40b6a3
Hubert Tubis <hubert.tubis@gmail.com>
2026-01-29 21:32:05 +0100

    -

--- src/App.java
+++ src/App.java
@@ -8,18 +8,23 @@ public class App {
         Database.database_initialize();
         WebScrapperGui.scrapperGui();
         WebScrapper.scrapper();
-        ResultGui.DisplayResult("pl,1;pl,0.3;");//code,liczba;
+        //ResultGui.DisplayResult("pl,1;pl,0.3;");//code,liczba;
     }
 
     public static void query_language(String letters_and_percentage){
-        Map<Double, String> map = new TreeMap<>();
+        Map<Double, String> map = new TreeMap<>(java.util.Collections.reverseOrder());
+        String result = "";
         for (String lan : WebScrapper.languageCodes){
             String lang_stats = Database.get_data_language(lan);
             if(!lang_stats.equals("null#")){
-                map.put(Statistics.pearson_test(lang_stats, letters_and_percentage), WebScrapper.localNames.get(lan));
-                System.out.println(Statistics.pearson_test(lang_stats, letters_and_percentage));
+                Double score = Statistics.pearson_test(lang_stats, letters_and_percentage);
+                map.put(score, lan);
             }
         }
+        for (Map.Entry<Double, String> entry : map.entrySet()) {
+            result += entry.getValue() + "," + entry.getKey() + ";";
+        }
+        ResultGui.DisplayResult(result);
     }
 
 
